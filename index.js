7826c1cd59328f46c58f2969fd8120130fcf87fc
fgarciamatar <fgarciamatar@gmail.com>
2025-06-16 20:12:01 -0300

    cambios

--- index.js
+++ index.js
@@ -3,60 +3,21 @@ const cors = require("cors");
 require("dotenv").config();
 const { connectDB } = require("./src/models/database");
 require("./src/jobs/inactiveUsers");
+
+
 const routes = require("./src/routes/index");
 const swaggerUi = require("swagger-ui-express");
 const swaggerSpec = require("./src/config/swagger");
 
 const app = express();
-
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://rendix-7iqvetyf0-fgarciamatars-projects.vercel.app"
-];
-
-// // Middleware CORS
-// app.use(cors({
-//   origin: function (origin, callback) {
-//     if (!origin || allowedOrigins.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   },
-//   credentials: true
-// }));
-
-// Preflight (OPTIONS) support
-app.options("*", cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true
-}));
-
-// // Manual CORS headers (backup y para seguridad extra)
-// app.use((req, res, next) => {
-//   const origin = req.headers.origin;
-//   if (allowedOrigins.includes(origin)) {
-//     res.setHeader("Access-Control-Allow-Origin", origin);
-//   }
-//   res.setHeader("Access-Control-Allow-Credentials", "true");
-//   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-//   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-//   next();
-// });
-
 app.use(cors({
-  origin: "*",
-  credentials: true
-}));
+    origin: [
+    "http://localhost:3000", // para desarrollo
+    "https://rendix.vercel.app", // frontend en Vercel
 
-
-
+  ],
+   credentials: true, 
+}))
 app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use("/", routes);
